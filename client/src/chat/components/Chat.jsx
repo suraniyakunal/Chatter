5ac42906d48b8fadc74a43d65fd50fc66ed52a40
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-07-31 08:02:28 +0530

    Updated the UI

--- client/src/chat/components/Chat.jsx
+++ client/src/chat/components/Chat.jsx
@@ -23,30 +23,28 @@ function Chat() {
 
 
   return (
-    <div className='min-h-screen min-w-full flex justify-center items-center bg-black text-white relative'>
-      <div id='chatbox' className='h-[90%] w-[90%] flex ring bg-gray-950 shadow-2xl shadow-gray-900 absolute'>
-        <div id='onlineUsers' className='h-full w-[25%] border-2'>
+    <div className='h-screen w-full flex justify-center items-center bg-black text-white relative'>
+      <div id='chatbox' className='h-[90%] w-[90%] flex ring rounded-md bg-gray-950 shadow-2xl shadow-gray-900 absolute'>
+        <div id='onlineUsers' className='h-full w-[25%] text-center'>
           <h1>Users</h1>
         </div>
-        <div id='chatSection' className='h-full w-full border-2 items-end'>
-          <form className='flex gap-5 justify-center items-center h-20 w-full '>
+        <div id="chatSection" className="flex flex-col h-full border-s-2">
+          <div className="flex-1 overflow-y-auto">
+            {/* Chat messages go here */}
+          </div>
+
+          <form className="flex gap-5 justify-center items-center h-20 w-full px-4 py-3 ">
             <input
               type="text"
-              name=""
-              value=""
-              placeholder='Type message'
-              className='
-              rounded-xl text-center
-              h-10 w-[650px] ml-12 p-3'
+              placeholder="Type message"
+              className="rounded-xl h-10 w-[650px] p-3 text-center"
             />
             <button
               type="submit"
-              className='
-              h-10 w-40 bg-amber-500
-              rounded-lg hover:bg-amber-600
-              hover:text-gray-600
-              hover:h-8 hover:w-35'
-            >Send</button>
+              className="h-10 w-40 bg-amber-500 rounded-lg hover:bg-amber-600 hover:text-gray-600"
+            >
+              Send
+            </button>
           </form>
         </div>
       </div>
