76b1d49d2a370f2c7d06421dc9f7ae5e5fd738a6
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-08-02 23:27:27 +0530

    Updated the UI

--- client/src/chat/components/Chat.jsx
+++ client/src/chat/components/Chat.jsx
@@ -13,35 +13,43 @@ function Chat() {
 
   useEffect(() => {
     socket.on('privateChat', (data) => {
-
+      setChat(prev => [...prev, data])
     })
 
     return () => {
-
+      socket.off('privateChat')
     }
   }, [])
 
+  const handleSendMessage = (e) => {
+    e.preventDefault()
+    socket.emit('privateChat', message)
+  }
 
   return (
     <div className='h-screen w-full flex justify-center items-center bg-black text-white relative'>
       <div id='chatbox' className='h-[90%] w-[90%] flex ring rounded-md bg-gray-950 shadow-2xl shadow-gray-900 absolute'>
-        <div id='onlineUsers' className='h-full w-[25%] text-center'>
+        <div id='onlineUsers' className='h-full w-[35%] text-center'>
           <h1>Users</h1>
         </div>
-        <div id="chatSection" className="flex flex-col h-full border-s-2">
+        <div id="chatSection" className="flex flex-col h-full w-full border-s-2">
           <div className="flex-1 overflow-y-auto">
-            {/* Chat messages go here */}
+            {chat.map((msg, id) => (
+              <li key={id}>{msg.message}</li>
+            ))}
           </div>
 
-          <form className="flex gap-5 justify-center items-center h-20 w-full px-4 py-3 ">
+          <form className="flex gap-4 justify-center items-center h-20 w-full px-4 py-3 ">
             <input
               type="text"
               placeholder="Type message"
-              className="rounded-xl h-10 w-[650px] p-3 text-center"
+              className="text-left rounded-xl h-10 w-[650px] ring p-3"
+              onChange={(e) => { setMessage(e.target.value) }}
             />
             <button
               type="submit"
               className="h-10 w-40 bg-amber-500 rounded-lg hover:bg-amber-600 hover:text-gray-600"
+              onClick={handleSendMessage}
             >
               Send
             </button>
