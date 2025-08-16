82396da0020c452bda833a8a684c7a2f9586ceed
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-08-16 23:58:12 +0530

    Updated UI

--- client/src/chat/components/Chat.jsx
+++ client/src/chat/components/Chat.jsx
@@ -12,6 +12,7 @@ function Chat() {
   useEffect(() => {
     socket.on('private-chat', (data) => {
       setChat(prev => [...prev, data])
+      console.log("vtdytduuyfyf")
     })
 
     socket.on('join-room', (data) => {
