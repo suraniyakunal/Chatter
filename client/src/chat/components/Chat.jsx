46b60d8ff2d4c20b8355008fcec8f9aeed066485
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-08-25 22:19:50 +0530

    improved Logic

--- client/src/chat/components/Chat.jsx
+++ client/src/chat/components/Chat.jsx
@@ -6,33 +6,43 @@ function Chat() {
 
   const [message, setMessage] = useState('')
   const [chat, setChat] = useState('')
-  const [room, setRoom] = useState([])
+  // const [room, setRoom] = useState([])
+  const [users, setUsers] = useState([])
 
 
   useEffect(() => {
-    socket.on('private-chat', (data) => {
+
+    socket.on('online-users', async (usr) => {
+      const names = await usr
+      setUsers(names)
+      console.log(users)
+    })
+    // for future features
+    socket.on('global-chat', (data) => {
       setChat(prev => [...prev, data])
     })
 
-    socket.on('join-room', (data) => {
-      setRoom(prev => [...prev, data])
-    })
-
-    socket.on('create-room', (room) => {
-      setRoom(prev => [...prev, room])
-    })
+    // // socket.on('join-room', (data) => {
+    // //   setRoom(prev => [...prev, data])
+    // // })
+    // //
+    //
+    // socket.on('create-room', (room) => {
+    //   setRoom(prev => [...prev, room])
+    // })
 
     return () => {
-      socket.off('private-chat')
-      socket.off('join-room')
-      socket.off('create-room')
+      socket.off('online-users')
+      socket.off('global-chat')
+      // socket.off('join-room')
+      // socket.off('create-room')
     }
-  }, [])
+  }, [users])
 
   const handleSendMessage = (e) => {
     e.preventDefault()
     socket.emit('private-chat', message)
-    socket.emit('join-room', room)
+    // socket.emit('join-room', room)
     socket.emit('welcome', 'Welcome to the app')
   }
 
@@ -40,12 +50,12 @@ function Chat() {
     <div className='h-screen w-full flex justify-center items-center bg-black text-white relative'>
       <div id='chatbox' className='h-[90%] w-[90%] flex ring rounded-md bg-gray-950 shadow-2xl shadow-gray-900 absolute'>
         <div id='onlineUsers' className='h-full w-[35%] text-center'>
-          <h1>Rooms</h1>
+          <h1>Users</h1>
           <ul className='w-full h-5'>
-            {room ? room.map((r, id) => (
-              <li key={id}>{r}</li>
+            {users ? users.map((r, id) => (
+              <li key={id}>{r.name}</li>
             )) :
-              <p>No rooms yet prevent</p>}
+              <p>No user Online</p>}
           </ul>
         </div>
         <div id="chatSection" className="flex flex-col h-full w-full border-s-2">
