fc20f28145bbd9f472419cf2ce8cb9739748a242
Kunal Suraniya <kunalsuraniya123@gmail.com>
2025-09-03 23:54:13 +0530

    updated something

--- client/src/chat/components/Chat.jsx
+++ client/src/chat/components/Chat.jsx
@@ -15,6 +15,11 @@ function Chat() {
   const addEmoji = (emoji) => {
     setMessage(prev => [...prev, emoji.native])
     console.log(emoji)
+    console.log(emoji)
+    console.log(emoji)
+    console.log(emoji)
+    console.log(emoji)
+    console.log(emoji)
   }
 
 
